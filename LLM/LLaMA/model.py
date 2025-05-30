d2b7c28dcebdafe2c72705a15380ccbdfa4d9bf2
ParkSoeon <106726934+ParkSoeon@users.noreply.github.com>
2025-05-30 12:29:58 +0900

    Update model.py

--- LLM/LLaMA/model.py
+++ LLM/LLaMA/model.py
@@ -96,15 +96,70 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids = None, unsqueeze_dim = 1)
     return q_embed, k_embed
 
 class LlamaMLP(nn.Module):
+    """
+    LlamaMLP is a Multi-Layer Perceptron (MLP) module used in the LLaMA model
+    
+    Attributes:
+    - config (LlamaConfig): Configuration object containing the model parameters.
+    - hidden_size (int): Size of the hidden layer.
+    - intermediate_size (int): Size of the intermediate layer.
+    - gate_proj (nn.Linear): Linear layer for the gate projection.
+    - up_proj (nn.Linear): Linear layer for the up projection.
+    - down_proj (nn.Linear): Linear layer for the down projection.
+    - act_fn (Callable): Activation function to be applied in the MLP.
+    """
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.hidden_size = config.hidden_size
+        self.intermediate_size = config.intermediate_size
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias = config.mlp_bias)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias = config.mlp_bias)
+        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias = config.mlp_bias)
+        self.act_fn =ACT2FN[config.hidden_act]
+
+        def forward(self, x):
+            down_proj = self.down_proj(self.act_fn(self.gate_proj(x) * self.up_proj(x)))
+            return down_proj
 
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
+    batch, num_key_value_heads, slen, head_dim = hidden_states.shape
+
+    if n_rep == 1:
+        return hidden_states
+    hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
+    return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
 
 def eager_attention_forward(
-
+    module: nn.Module,
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    attention_mask: Optional[torch.Torch],
+    scaling: flaot,
+    dropout: float = 0.0,
+    **kwargs
 ):
+    key_states = repeat_kv(key, module.num_key_value_groups) # Repeat the key states for each key-value group
+    value_states = repeat_kv(value, module.num_key_value_groups) # Repeat the value states for each key-value group
+
+    attn_weights = torch.matmul(query, key_states.transpose(2, 3)) * scaling
+    if attention_mask is not None:
+        causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+        attn_weights = attn_weights + causal_mask
+        
+    attn_weights = nn.functional.softmax(attn_weights, dim = -1, dtype = torch.float32).to(query.dtype) # Apply softmax to the attention weights
+    attn_weights = nn.functional.dropout(attn_weights, p = dropout, training = module.training) # Apply dropout to the attention weights
+    attn_output = torch.matmul(attn_wights, value_states)
+    attn_output = attn_output.transpose(1, 2).contiguous() #.view(query.shape[0], -1, module.config.hidden_size) # Reshape the attention output to (batch_size, seq_length, hidden_size)
+
+    return attn_output, attn_weights
 
 class LlamaAttention(nn.Module):
-    """ Has Multi-headed Attention from "Attention is All You Need" paper."""
+    """ 
+    Multi-Headed Attention Module with support for Multi-Query Attention (MQA)
+    Rotary Positional Embeddings (RoPE), and Multiple Attention backends(eager, flash, etc.)
+    """
 
     def __init__(self, config: LlamaConfig, layer_idx: int):
         super().__init__()
@@ -128,20 +183,26 @@ class LlamaAttention(nn.Module):
 
         - bias: Whether to use bias in the projection layers
         """
+        # Projection Layers 
         self.q_proj = nn.Linear(config.hidden_size, config.num_attention_heads * self.head_dim, bias = config.attention_bias)
         self.k_proj = nn.Linear(config.hidden_size, config.num_key_value_heads * self.head_dim, bias = config.attention_bias)
         self.v_proj = nn.Linear(config.hidden_size, config.num_key_value_heads * self.head_dim, bias = config.attention_bias)
         self.o_proj = nn.Linear(config.num_attention_heads * self.head_dim, config.hidden_size, bias = config.attention_bias)
     
     """
-    def forward(): 
-        Forward pass of a multi-head attention layer with rotary embeddings and optional caching
-        - hidden_states: Input tensor of shape (batch_size, seq_length, hidden_size)
-        - position_embeddings: Tuple containing the cosine and sine embeddings for the rotary positional embedding
-        - attention_mask: Optional tensor for masking attention scores
-        - past_key_value: Optional cache for past key and value states
-        - cache_position: Optional tensor for the position in the cache
-        - **kwargs: Additional keyword arguments for flash attention
+    def forward():
+        Args:
+            hidden_states (torch.Tensor): Input tensor of shape (batch_size, seq_len, hidden_size)
+            position_embeddings (Tuple[Tensor, Tensor]): Cosine and sine embeddings for RoPE
+            attention_mask (Optional[Tensor]): Masking tensor for attention
+            past_key_value (Optional[Cache]): Cached key/value tensors for KV cache
+            cache_position (Optional[LongTensor]): Position in cache
+            **kwargs: Additional arguments (e.g., FlashAttention-specific)
+        Returns:
+            Tuple:
+                attn_output (Tensor): Output tensor of shape (batch, seq_len, hidden_size)
+                attn_weights (Optional[Tensor]): Attention weights (if requested)
+                past_key_value (Optional[Tuple[Tensor]]): Updated cache for inference
     """
     def forward(
         self,
@@ -155,10 +216,12 @@ class LlamaAttention(nn.Module):
         input_shape = hidden_states.shape[:-1]
         hidden_shape = (*input_shape, -1, self.head_dim)
 
+        # Linear Projections and Reshape([Bacth_Size, Seq_Length, Num_Heads])
         query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1, 2)
         key_states = self.k_proj(hidden_states).view(hidden_shape).transpose(1, 2)
         value_states = self.v_proj(hidden_states).view(hidden_shape).transpose(1, 2)
 
+        # Apply RoPE(Rotary Positional Embedding)
         cos, sin = position_embeddings
         query_states, key_states = apply_rotary_por_emb(query_states, key_states, cos, sin)
 
@@ -187,6 +250,55 @@ class LlamaAttention(nn.Module):
         
 
 class LlamaDecoderLayer(GradientCheckpointingLayer):
+    def __init__(self, config: LlamaConfig, layer_idx: int):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+        
+        self.self_attn = LlamaAttention(config = config, layer_idx = layer_idx)
+
+        self.mlp = LlamaMLP(config)
+        self.input_layernorm = LlamaRMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        pask_key_value: Optional[Cache] = None,
+        output_attentions: Optional[bool] = False,
+        use_cache: Optional[bool] = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, tensor.Tensor]] = None,
+        **kwargs: Unpack[FlashAttentionKwargs],
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+        residual = hidden_states
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # Self Attention
+        hidden_states, self_attn_weight = self.self_attn(
+            hidden_states = hidden_states,
+            attention_mask = attention_mask,
+            positio_ids = position_ids,
+            past_key_value = past_key_value,
+            output_attentions = output_attentions,
+            use_cache = use_cache,
+            cache_position = cache_position,
+            position_embeddings = position_embeddings,
+            **kwargs
+        )
+        hidden_states = residual + hidden_states
+
+        # Fully Connected
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states = self.mlp(hidden_states)
+        hidden_states = residual + hidden_states
+
+        outputs = (hidden_states,)
+        if output_attentions:
+            outputs += (self_attn_weights,)
+        return outputs
+
 
 @auto_docstring
 class LlamaPreTrainedModel(PreTrainedModel):
