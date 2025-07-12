84d4cb6d1da526ce8aca76aed9efdddcad9aa76a
ParkSoeon <106726934+ParkSoeon@users.noreply.github.com>
2025-07-12 16:40:43 +0900

    Update compute_perplexity.py

--- Evaluations/getAutoGT/compute_perplexity.py
+++ Evaluations/getAutoGT/compute_perplexity.py
@@ -1,3 +1,4 @@
+# ./compute_perplexity.py
 import torch
 from utils import device, MAX_PROMPT, MAX_CONTEXT, top_k
 from tqdm import tqdm 
@@ -78,44 +79,3 @@ class getPerplexity:
         """
         prompt = self.build_context_prompt(context, question, examples)
         return self.compute_perplexity(prompt, answer)
-
-# class ContextAnalyze:
-#     def __init__(self, getPerplexity):
-#         self.getPPL = getPerplexity
-
-#     def find_best_context(self, question: str, answer: str, rules: list, examples: list, top_k: int=top_k) -> tuple:
-#         baseline_ppl = self.getPPL.compute_baseline_ppl(question, answer, examples)
-
-#         context_results = []
-
-#         print("Computing context PPL...")
-#         for r in tqdm(rules):
-#             context_ppl = self.getPPL.compute_context_ppl(r["description"], question, answer, examples)
-
-#             delta_ppl = baseline_ppl - context_ppl
-#             if delta_ppl > 0:
-#                 improvement_ratio = delta_ppl / baseline_ppl
-#             else:
-#                 improvement_ratio = 0
-            
-#             context_results.append({
-#                 "rule_id": r["rule_id"],
-#                 "category": r["category"],
-#                 "title": r["title"],
-#                 "description": r["description"],
-#                 "context_ppl": context_ppl,
-#                 "baseline_ppl": baseline_ppl,
-#                 "delta_ppl": delta_ppl,
-#                 "improvement_ratio": improvement_ratio
-#             })
-
-#         # Sort contexts by low perplexity
-#         context_results_by_ppl = sorted(context_results, key = lambda x: x["context_ppl"])
-#         lowest_ppl_contexts = context_results_by_ppl[:top_k] # Which represents the best contexts were found for the answer
-
-#         # Sort contexts by high delta perplexity(which means the context improved the most)
-#         positive_delta_results = [r for r in context_results if r["delta_ppl"] > 0]
-#         context_results_by_delta = sorted(positive_delta_results, key = lambda x: x["delta_ppl"], reverse=True)
-#         highest_delta_contexts = context_results_by_delta[:top_k]
-
-#         return lowest_ppl_contexts, highest_delta_contexts, baseline_ppl
